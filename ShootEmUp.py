a3618bc788e2c3e03e7106ce6e3a0921a9eb28b5
bunsalcoder <bunsal.hul@student.passerellesnumeriques.org>
2021-03-02 11:22:09 +0700

    Adding to github

--- ShootEmUp.py
+++ ShootEmUp.py
@@ -1,7 +1,6 @@
 #___import______________________________________________________________________________________
 
 from tkinter import *
-import time
 import random
 
 
@@ -16,6 +15,9 @@ root.title("Shoot 'Em Up")
 top = Canvas(root, width = 1200, height = 50, bg = "black")
 top.grid(column = 0, row = 0)
 
+
+#___Playground__________________________________________________________________________________
+
 playground = Canvas(root, width = 1200, height = 1000, bg = "white")
 playground.grid(column = 0, row = 1)
 
@@ -25,15 +27,19 @@ playground.grid(column = 0, row = 1)
 # Score!!
 
 score = 0
-scoreLabel = Label(top, text = "Score: " + str(score), bg = "green", fg = "white", font = (50))
-scoreLabel.place(x = 50, y = 15)
+scoreLabel = Label(top, text = "Score: " + str(score), bg = "black", fg = "blue", font = ("Sans",  25))
+scoreLabel.place(x = 50, y = 5)
 
 
 # Lives!!
 
-lives = 3
-livesLabel = Label(top, text = "Lives Left: " + str(lives), bg = "red", fg = "white", font = (50))
-livesLabel.place(x = 1040, y = 15)
+spaceLiveImage = PhotoImage(file = "heart.png")
+myLives1 = Label(top, image = spaceLiveImage, bg = "black")
+myLives1.place(x = 1050, y = 5)
+myLives2 = Label(top, image = spaceLiveImage, bg = "black")
+myLives2.place(x = 1100, y = 5)
+myLives3 = Label(top, image = spaceLiveImage, bg = "black")
+myLives3.place(x = 1150, y = 5)
 
 
 #___Images______________________________________________________________________________________
@@ -43,7 +49,6 @@ livesLabel.place(x = 1040, y = 15)
 backgroundImage = PhotoImage(file = "background.png")
 myBackground = playground.create_image(598, 500, image = backgroundImage)
 
-
 # Spaceship image!!
 
 spaceShipImage = PhotoImage(file = "spaceInvader.png")
@@ -52,9 +57,6 @@ myShip = playground.create_image(600, 750, image = spaceShipImage)
 # Monster image!!
 
 monsterImage = PhotoImage(file = "monster.png")
-theMonster1 = playground.create_image(50, 50, image = monsterImage)
-theMonster2 = playground.create_image(600, 50, image = monsterImage)
-theMonster3 = playground.create_image(1150, 50, image = monsterImage)
 
 # Bullet image!!
 
@@ -63,77 +65,123 @@ bulletImage = PhotoImage(file = "bullet.png")
 
 #___Monster Movement_____________________________________________________________________________
 
-    
+monsterList = []
+numberOfLife = 0
 
-#___Bullet Function_______________________________________________________________________________
+def createMonster():
+    X = random.randrange(80, 1120)
+    Y = -80
+    monsters = playground.create_image(X, Y, image = monsterImage)
+    monsterList.append(monsters)
+    playground.after(1000, createMonster)
 
-def shootEnemy():
-    global leftBullet, rightBullet
-    myLeftBullet = playground.move(leftBullet, 0, -15)
-    myRightBullet = playground.move(rightBullet, 0, -15)
+def moveMonster():
+    for monster in range(len(monsterList)):
+        axeY = playground.coords(monsterList[monster])[1]
+        monsters = monsterList[monster]
+        playground.move(monsters, 0, 50)
+        if playground.coords(monsters)[1] + 40 == playground.coords(myShip)[1] - 75:
+            numberOfLife += 1
+            lifeLess()
+            
+    playground.after(320, lambda: moveMonster())
+
+
+def lifeLess():
+    if numberOfLife == 1:
+        playground.delete(myLives1)
+    elif numberOfLife == 2:
+        playground.delete(myLives2)
+    elif numberOfLife == 3:
+        playground.delete(myLives3)
+
+
+#___Bullet Movement_______________________________________________________________________________
+
+bulletList = []
+
+def shootMonster():
+    global bulletList
+    myLeftBullet = playground.move(leftBullet, 0, -20)
+    myMidBullet = playground.move(midBullet, 0, -20)
+    myRightBullet = playground.move(rightBullet, 0, -20)
 
     myY = playground.coords(leftBullet)[1]
     beyondShooting = myY <= 0
-    
-    if not beyondShooting:
-        playground.after(10, lambda: shootEnemy())
-    else:
-        playground.delete(leftBullet)
-        playground.delete(rightBullet)
-        toShoot()
+    monsterBulletToDelete = monsterBullet()
+    if len(monsterBulletToDelete) == 0:    
+        if not beyondShooting:
+            playground.after(10, lambda:shootMonster())
+        else:
+            playground.delete(leftBullet)
+            playground.delete(midBullet)
+            playground.delete(rightBullet)
+            toShoot()
 
+
+ 
 
 def toShoot():
-    global myShip, leftBullet, rightBullet
+    global myShip, leftBullet, rightBullet, midBullet, bulletList
     X1 = playground.coords(myShip)[0]
     Y1 = playground.coords(myShip)[1]
     leftBullet = playground.create_image(X1 - 40, Y1 + 35, image = bulletImage)
+    midBullet = playground.create_image(X1, Y1 - 20, image = bulletImage)
     rightBullet = playground.create_image(X1 + 40, Y1 + 35, image = bulletImage)
-    shootEnemy()
+    bulletList = [leftBullet, midBullet, rightBullet]
+    shootMonster()
 
+
+#___ImpactMonsterBullet____________________________________________________________________________
+
+def monsterBullet():           # Function use to check the collision of bullet and the monster
+    deleteMonsterBullet = []
+    for bullet in bulletList:
+        bulletPosition = playground.coords(bullet)
+        for theMonster in monsterList:
+            monsterPosition = playground.coords(theMonster)
+            if (bulletPosition[1] <= monsterPosition[1] + 80) and (((bulletPosition[0] >= monsterPosition[0]) and (bulletPosition[0] <= monsterPosition[0] + 80)) or ((bulletPosition[0] + 50 >= monsterPosition[0]) and (bulletPosition[0]+50 <= monsterPosition[0] + 80))):
+                deleteMonsterBullet.append(bullet) 
+                deleteMonsterBullet.append(theMonster)
+    return deleteMonsterBullet
+
+
+def eraseBulletMonster():      # Function to remove bullet and the monster from the list
+    monsterBulletToDelete = monsterBullet()
+    if len(monsterBulletToDelete) > 0:
+        bulletList.remove(monsterBulletToDelete[0])
+        monsterList.remove(monsterBulletToDelete[1])
+        playground.delete(monsterBulletToDelete[0])
+        playground.delete(monsterBulletToDelete[1])
+        
+    playground.after(10, eraseBulletMonster)
 
 
 #___Ship Movement_________________________________________________________________________________
 
-# Move left!!
-
-def moveLeft(event):
-    if playground.coords(myShip)[0] > 60:
-        playground.move(myShip, -20, 0)
-
-
-# Move right!!
-
-def moveRight(event):
-    if playground.coords(myShip)[0] < 1140:
-        playground.move(myShip, 20, 0)
-
-
-# Move up!!
-
-def moveUp(event):
-    playground.move(myShip, 0, -20)
-
-
-# Move down!!
-
-def moveDown(event):
-    if playground.coords(myShip)[1] < 880:
-        playground.move(myShip, 0, 20)
-
-
+def shipMove(event):
+    if  playground.coords(myShip)[0] <= 1125 and event.char == "d":
+        playground.move(myShip, 30, 0)
+    elif playground.coords(myShip)[0] >= 75 and event.char == "a":
+        playground.move(myShip, -30, 0)
+    elif playground.coords(myShip)[1] > 100 and event.char == "w":
+        playground.move(myShip, 0, -30)
+    elif playground.coords(myShip)[1] < 830 and event.char == "s":
+        playground.move(myShip, 0, 30)
+    
 
 #___Function Call_______________________________________________________________________________
 
-toShoot()
+createMonster()         # This is to create monster
+moveMonster()           # This is to move the monster
+toShoot()               # This is to shoot the monster
+eraseBulletMonster()    # This is to delete bullet and monster from the list
+lifeLess()              # This is to decrease the life
 
 
 #___Key event___________________________________________________________________________________
 
-playground.bind_all("<a>", moveLeft)
-playground.bind_all("<d>", moveRight)
-playground.bind_all("<w>", moveUp)
-playground.bind_all("<s>", moveDown)
+root.bind("<Key>", shipMove)
 
 
 #___At the very bottom__________________________________________________________________________
